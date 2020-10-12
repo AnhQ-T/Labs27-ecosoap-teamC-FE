798005b635e0df12714351efba8030176136e04e
Karen Lei <karenwinnielei@gmail.com>
2020-10-12 10:33:29 -0700

    add validation to form inputs and require input type for number of soap bars requested and number of beneficiaries to be number

--- src/components/orders/OrderFormContainer.js
+++ src/components/orders/OrderFormContainer.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Form, Input, Button, Tooltip } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Button, Tooltip, InputNumber } from 'antd';
 
 import { QuestionCircleOutlined } from '@ant-design/icons';
 
@@ -8,9 +8,18 @@ import 'antd/dist/antd.css';
 const OrderFormContainer = () => {
   const [form] = Form.useForm();
   const [formLayout, setFormLayout] = useState('horizontal');
+  const [, forceUpdate] = useState();
 
   const onFormLayoutChange = ({ layout }) => {
     setFormLayout(layout);
+  };
+
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  const onFinish = values => {
+    console.log('Finish:', values);
   };
 
   const formItemLayout = {
@@ -39,24 +48,61 @@ const OrderFormContainer = () => {
           layout: formLayout,
         }}
         onValuesChange={onFormLayoutChange}
+        onFinish={onFinish}
       >
-        <Form.Item name="organizationName" label="Organization Name" required>
+        <Form.Item
+          name="organizationName"
+          label="Organization Name"
+          required
+          rules={[
+            {
+              required: true,
+              message: 'Please input your organization name',
+            },
+          ]}
+        >
           <Input placeholder="Organization Name" />
         </Form.Item>
         <Form.Item name="organizationWebsite" label="Organization Website">
           <Input placeholder="Organization Website" />
         </Form.Item>
-        <Form.Item name="contactName" label="Contact Name" required>
+        <Form.Item
+          name="contactName"
+          label="Contact Name"
+          required
+          rules={[
+            {
+              required: true,
+              message: 'Please input your contact name',
+            },
+          ]}
+        >
           <Input placeholder="Contact Name" />
         </Form.Item>
         <Form.Item
           name="soapBarNum"
           label="Number of Soap Bars Requested"
           required
+          rules={[
+            {
+              required: true,
+              message: 'Please input number of soap bars requested',
+            },
+          ]}
         >
-          <Input placeholder="Number of Soap Bars Requested" />
+          <InputNumber placeholder="Number of Soap Bars Requested" min={1} />
         </Form.Item>
-        <Form.Item name="contactPhone" label="Contact Phone Number" required>
+        <Form.Item
+          name="contactPhone"
+          label="Contact Phone Number"
+          required
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number',
+            },
+          ]}
+        >
           <Input placeholder="Contact Phone Number" />
         </Form.Item>
         <Form.Item
@@ -66,11 +112,11 @@ const OrderFormContainer = () => {
           rules={[
             {
               type: 'email',
-              message: 'The input is not valid E-mail!',
+              message: 'The input is not a valid email',
             },
             {
               required: true,
-              message: 'Please input your E-mail!',
+              message: 'Please input your email address',
             },
           ]}
         >
@@ -79,15 +125,31 @@ const OrderFormContainer = () => {
         <Form.Item name="address" label="Address / Location">
           <Input placeholder="Address / Location" />
         </Form.Item>
-        <Form.Item name="country" label="Country" required>
+        <Form.Item
+          name="country"
+          label="Country"
+          required
+          rules={[
+            {
+              required: true,
+              message: 'Please input country',
+            },
+          ]}
+        >
           <Input placeholder="Country" />
         </Form.Item>
         <Form.Item
           name="beneficiariesNum"
           label="Number of Beneficiaries"
           required
+          rules={[
+            {
+              required: true,
+              message: 'Please input number of beneficiaries',
+            },
+          ]}
         >
-          <Input placeholder="Number of Beneficiaries" />
+          <InputNumber placeholder="Number of Beneficiaries" min={1} />
         </Form.Item>
         <Form.Item
           name="hygieneSituation"
@@ -99,6 +161,12 @@ const OrderFormContainer = () => {
           name="hygieneInitiative"
           label="Description of Hygiene Initiative"
           required
+          rules={[
+            {
+              required: true,
+              message: 'Please input description of hygiene initiative',
+            },
+          ]}
         >
           <Input placeholder="Description of Hygiene Initiative" />
         </Form.Item>
@@ -106,7 +174,16 @@ const OrderFormContainer = () => {
           <Input placeholder="Comments" />
         </Form.Item>
         <Form.Item {...buttonItemLayout}>
-          <Button type="primary">Submit Order</Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            // disabled={
+            //   !form.isFieldsTouched(true) ||
+            //   form.getFieldsError().filter(({ errors }) => errors.length).length
+            // }
+          >
+            Submit Order
+          </Button>
         </Form.Item>
       </Form>
     </>
