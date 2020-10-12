22fdb628ec411fc96e85dac3a450b120945d4f3b
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-10-12 12:12:13 -0400

    installed redux and react-redux packages for state management

---
+++ src/components/Dashboard/OrderList/OrderList.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import axios from 'axios';
+
+import { buyer } from '../../../state/actions/index';
+
+import 'antd/dist/antd.css';
+
+function OrderList(props) {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default OrderList;
--- src/components/Dashboard/OrderList/OrderListContainer.js
+++ src/components/Dashboard/OrderList/OrderListContainer.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import axios from 'axios';
 
 import 'antd/dist/antd.css';
 
+import OrderList from './OrderList.js';
+
 function OrderListContainer() {
-  return <div></div>;
+  return (
+    <div>
+      <OrderList />
+    </div>
+  );
 }
 
 export default OrderListContainer;
