fbc4442fa6cf5059b1df11b995d07516a1228380
Karen Lei <karenwinnielei@gmail.com>
2020-10-13 14:40:33 -0700

    get axios.post working

--- src/components/orders/OrderFormContainer.js
+++ src/components/orders/OrderFormContainer.js
@@ -41,14 +41,15 @@ const OrderFormContainer = () => {
     forceUpdate({});
   }, []);
 
-  const onFinish = e => {
-    console.log();
+  const onFinish = values => {
+    console.log(form.getFieldsValue());
+    console.log(order);
     axios
-      .post('https://labs27-ecosoap-teamc-api.herokuapp.com/orders/')
+      .post('https://labs27-ecosoap-teamc-api.herokuapp.com/orders/', values)
       .then(res => {
         console.log(res);
-        setOrder(order);
-        // form.setFieldsValue(order)
+        // setOrder(order);
+        form.setFieldsValue(order);
       })
       .catch(err => console.log(`Order form error: ${err}`));
   };
@@ -70,174 +71,176 @@ const OrderFormContainer = () => {
   };
 
   return (
-    <Form.Provider onFormFinish={onFinish} value={form}>
-      <Form
-        {...formItemLayout}
-        layout={formLayout}
-        form={form}
-        initialValues={{
-          layout: formLayout,
-        }}
-        onValuesChange={onFormLayoutChange}
-        // onFinish={onFinish}
+    <Form
+      {...formItemLayout}
+      layout={formLayout}
+      form={form}
+      initialValues={{
+        layout: formLayout,
+      }}
+      onValuesChange={onFormLayoutChange}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="organizationName"
+        label="Organization Name"
+        // valuePropName="organizationName"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input organization name',
+          },
+        ]}
       >
-        <Form.Item
-          name="organizationName"
-          label="Organization Name"
-          valuePropName="organizationName"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input organization name',
-            },
-          ]}
-        >
-          <Input placeholder="Organization Name" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="organizationWebsite"
-          label="Organization Website"
-          valuePropName="organizationWebsite"
-        >
-          <Input placeholder="Organization Website" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="contactName"
-          label="Contact Name"
-          valuePropName="contactName"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input contact name',
-            },
-          ]}
-        >
-          <Input placeholder="Contact Name" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="soapBarNum"
-          label="Number of Soap Bars Requested"
-          valuePropName="soapBarNum"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input number of soap bars requested',
-            },
-          ]}
-        >
-          <InputNumber
-            placeholder="Number of Soap Bars Requested"
-            min={1}
-            // onChange={inputChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="contactPhone"
-          label="Contact Phone Number"
-          valuePropName="contactPhone"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input phone number',
-            },
-          ]}
-        >
-          <Input placeholder="Contact Phone Number" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="contactEmail"
-          label="Contact Email Address"
-          // valuePropName="contactEmail"
-          required
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not a valid email',
-            },
-            {
-              required: true,
-              message: 'Please input email address',
-            },
-          ]}
-        >
-          <Input placeholder="Contact Email Address" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item name="address" label="Address / Location">
-          <Input placeholder="Address / Location" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="country"
-          label="Country"
-          valuePropName="country"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input country',
-            },
-          ]}
-        >
-          <Input placeholder="Country" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item
-          name="beneficiariesNum"
-          label="Number of Beneficiaries"
-          valuePropName="beneficiariesNum"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input number of beneficiaries',
-            },
-          ]}
-        >
-          <InputNumber
-            placeholder="Number of Beneficiaries"
-            min={1}
-            // onChange={inputChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="hygieneSituation"
-          label="Description of Hygiene Situation in Community"
-          valuePropName="hygieneSituation"
-        >
-          <Input
-            placeholder="Description of Hygiene Situation in Community"
-            onChange={inputChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="hygieneInitiative"
-          label="Description of Hygiene Initiative"
-          valuePropName="hygieneInitiative"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'Please input description of hygiene initiative',
-            },
-          ]}
-        >
-          <Input
-            placeholder="Description of Hygiene Initiative"
-            onChange={inputChange}
-          />
-        </Form.Item>
-        <Form.Item name="comments" label="Comments" valuePropName="comments">
-          <Input placeholder="Comments" onChange={inputChange} />
-        </Form.Item>
-        <Form.Item {...buttonItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit Order
-          </Button>
-        </Form.Item>
-      </Form>
-    </Form.Provider>
+        <Input placeholder="Organization Name" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="organizationWebsite"
+        label="Organization Website"
+        // valuePropName="organizationWebsite"
+      >
+        <Input placeholder="Organization Website" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="contactName"
+        label="Contact Name"
+        // valuePropName="contactName"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input contact name',
+          },
+        ]}
+      >
+        <Input placeholder="Contact Name" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="soapBarNum"
+        label="Number of Soap Bars Requested"
+        // valuePropName="soapBarNum"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input number of soap bars requested',
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Number of Soap Bars Requested"
+          // onChange={inputChange}
+          min={1}
+        />
+      </Form.Item>
+      <Form.Item
+        name="contactPhone"
+        label="Contact Phone Number"
+        // valuePropName="contactPhone"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input phone number',
+          },
+        ]}
+      >
+        <Input placeholder="Contact Phone Number" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="contactEmail"
+        label="Contact Email Address"
+        // valuePropName="contactEmail"
+        required
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not a valid email',
+          },
+          {
+            required: true,
+            message: 'Please input email address',
+          },
+        ]}
+      >
+        <Input placeholder="Contact Email Address" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item name="address" label="Address / Location">
+        <Input placeholder="Address / Location" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="country"
+        label="Country"
+        // valuePropName="country"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input country',
+          },
+        ]}
+      >
+        <Input placeholder="Country" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item
+        name="beneficiariesNum"
+        label="Number of Beneficiaries"
+        // valuePropName="beneficiariesNum"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input number of beneficiaries',
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Number of Beneficiaries"
+          min={1}
+          // onChange={inputChange}
+        />
+      </Form.Item>
+      <Form.Item
+        name="hygieneSituation"
+        label="Description of Hygiene Situation in Community"
+        // valuePropName="hygieneSituation"
+      >
+        <Input
+          placeholder="Description of Hygiene Situation in Community"
+          onChange={inputChange}
+        />
+      </Form.Item>
+      <Form.Item
+        name="hygieneInitiative"
+        label="Description of Hygiene Initiative"
+        // valuePropName="hygieneInitiative"
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Please input description of hygiene initiative',
+          },
+        ]}
+      >
+        <Input
+          placeholder="Description of Hygiene Initiative"
+          onChange={inputChange}
+        />
+      </Form.Item>
+      <Form.Item
+        name="comments"
+        label="Comments"
+        // valuePropName="comments"
+      >
+        <Input placeholder="Comments" onChange={inputChange} />
+      </Form.Item>
+      <Form.Item {...buttonItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit Order
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
