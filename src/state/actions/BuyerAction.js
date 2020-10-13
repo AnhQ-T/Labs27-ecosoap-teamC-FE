fea5f4c9e20b6b52e4f4e4b6c7c994daa97ced62
Karen Lei <karenwinnielei@gmail.com>
2020-10-13 10:22:09 -0700

    add POST_BUYER_ORDERS and POST_BUYER_ORDERS_ERROR

--- src/state/actions/BuyerAction.js
+++ src/state/actions/BuyerAction.js
@@ -19,3 +19,23 @@ export const getBuyerOrders = () => async dispatch => {
     });
   }
 };
+
+export const POST_BUYER_ORDERS = 'POST_BUYER_ORDERS';
+export const POST_BUYER_ORDERS_ERROR = 'POST_BUYER_ORDERS_ERROR';
+
+export const postBuyerOrders = () => async dispatch => {
+  try {
+    const res = await axios.post(
+      'https://labs27-ecosoap-teamc-api.herokuapp.com/orders'
+    );
+    dispatch({
+      type: POST_BUYER_ORDERS,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: POST_BUYER_ORDERS_ERROR,
+      payload: console.log(e),
+    });
+  }
+};
