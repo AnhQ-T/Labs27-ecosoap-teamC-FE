82e08bbcfe10f53985b2c26441a5d14032d11bb0
Karen Lei <karenwinnielei@gmail.com>
2020-10-14 16:03:25 -0700

    get order confirmation page to display order details and to send data to backend qualify endpoint

--- src/components/orders/OrderConfirmation.js
+++ src/components/orders/OrderConfirmation.js
@@ -1,32 +1,62 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const OrderConfirmation = props => {
-  // const initialOrder = {
-  //   organizationName: '',
-  //   organizationWebsite: '',
-  //   contactName: '',
-  //   soapBarNum: '',
-  //   contactPhone: '',
-  //   contactEmail: '',
-  //   address: '',
-  //   country: '',
-  //   beneficiariesNum: '',
-  //   hygieneSituation: '',
-  //   hygieneInitiative: '',
-  //   comments: '',
-  // };
+  const {
+    organizationName,
+    organizationWebsite,
+    contactName,
+    soapBarNum,
+    contactPhone,
+    contactEmail,
+    address,
+    country,
+    beneficiariesNum,
+    hygieneSituation,
+    hygieneInitiative,
+    comments,
+  } = props.location.state.values;
 
-  // const [order, setOrder] = useState(initialOrder);
-  // setOrder(props.state.values)
+  const { push } = useHistory();
+
+  const confirmOrder = values => {
+    console.log(props.location.state.values);
+    axios
+      .post(
+        'http://98.242.245.160:8000/orders/qualify',
+        props.location.state.values
+      )
+      .then(res => {
+        console.log(res.data);
+        if (res.data.checkIfPrice.hasPrice === true) {
+          push({
+            pathname: '/checkout',
+          });
+        } else {
+          console.log('message');
+        }
+      })
+      .catch(err => console.log(`Order confirmation error: ${err}`));
+  };
 
   return (
-    console.log(props.state.values),
-    (
-      <>
-        <h1>Order Confirmation</h1>
-        {/* {props.state.values.organizationWebsite} */}
-      </>
-    )
+    <>
+      <h1>Order Confirmation</h1>
+      <p>Organization Name: {organizationName}</p>
+      <p>Organization Website: {organizationWebsite}</p>
+      <p>Contact Name: {contactName}</p>
+      <p>Number of Soap Bars Requested: {soapBarNum}</p>
+      <p>Contact Phone: {contactPhone}</p>
+      <p>Contact Email: {contactEmail}</p>
+      <p>Address: {address}</p>
+      <p>Country: {country}</p>
+      <p>Number of Beneficiaries: {beneficiariesNum}</p>
+      <p>Hygiene Situation: {hygieneSituation}</p>
+      <p>Hygiene Initiative: {hygieneInitiative}</p>
+      <p>Comments: {comments}</p>
+      <button onClick={() => confirmOrder()}>Confirm Order</button>
+    </>
   );
 };
 
