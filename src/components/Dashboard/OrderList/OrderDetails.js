0b54ec9967305b46b7c9177be730fd2684d60096
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-10-15 13:56:29 -0400

    order details now populated with data

--- src/components/Dashboard/OrderList/OrderDetails.js
+++ src/components/Dashboard/OrderList/OrderDetails.js
@@ -3,10 +3,13 @@ import { useParams } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { getBuyerOrderDetails } from '../../../state/actions/BuyerAction.js';
+import '../../../styles/Dashboard/Orders/OrderDetails.css';
 
 import 'antd/dist/antd.css';
+import { Layout, Card, Typography } from 'antd';
 
 function OrderDetails(props) {
+  const { Title } = Typography;
   const { getBuyerOrderDetails } = props;
   const { id } = useParams();
 
@@ -14,9 +17,53 @@ function OrderDetails(props) {
     getBuyerOrderDetails(id);
   }, []);
 
+  console.log(props.current_order);
+
   return (
     <div>
-      <h2>Order Details</h2>
+      <Card
+        type="inner"
+        title={<Title level={3}>Order Details</Title>}
+        extra={
+          <div>
+            <h3>Order Id</h3>
+            <h4>{props.current_order.id}</h4>
+          </div>
+        }
+      >
+        <div className="order-details-container">
+          <div>
+            <h3>{props.current_order.contactName}</h3>
+            {props.current_order.contactEmail ? (
+              <h3>{props.current_order.contactEmail}</h3>
+            ) : null}
+            <h3>{props.current_order.contactPhone}</h3>
+            {props.current_order.address ? (
+              <h3>
+                {props.current_order.address} {props.current_order.country}{' '}
+              </h3>
+            ) : (
+              <h3>{props.current_order.country}</h3>
+            )}
+          </div>
+          <div>
+            <h3>{props.current_order.organizationName}</h3>
+            {props.current_order.organizationWebsite ? (
+              <h3>{props.current_order.organizationWebsite}</h3>
+            ) : null}
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div className="order-summary">
+          <h2>Order Summary</h2>
+          <h3>Soap Bars Ordered: {props.current_order.soapBarNum}</h3>
+          <h3>
+            Number of Beneficiaries: {props.current_order.beneficiariesNum}
+          </h3>
+          <h3>Comments: {props.current_order.beneficiariesNum}</h3>
+        </div>
+      </Card>
     </div>
   );
 }
