731dd7645c59f403171d36f6b3c4f5b06c59addf
Karen Lei <karenwinnielei@gmail.com>
2020-10-20 23:41:46 -0700

    center login form and make button font bolder

--- src/components/pages/Login/BuyerLogin.js
+++ src/components/pages/Login/BuyerLogin.js
@@ -37,7 +37,7 @@ const BuyerLogin = props => {
       </div>
       <Form
         // name="normal_login"
-        // className="login-form"
+        className="login-form"
         initialValues={{
           remember: true,
         }}
